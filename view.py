bcadfacb1367c7927f2da84737b4b6a4a07f3865
RobertMarting <robbiemarting@yahoo.com>
2022-04-08 15:50:17 -0700

    click
    Added the ability to click to put tokens on the board

--- view.py
+++ view.py
@@ -10,52 +10,77 @@ from tkinter import Checkbutton
 from tkinter import Frame
 
 
-class GameBoardView(tk.Frame):
+from controller import ConnectFourController
+from model import ConnectFourModel
 
+class GameBoardView(tk.Frame):
+    
+    
+    controller = 0;
+    
+    root = Tk();
+    root.title("Connect Four");
+    
+    
+    gameFrame = Frame(root);
+    gameFrame.grid(row = 0, column = 0);
+    
+    
+    gameCanvas = Canvas(gameFrame, bg = 'blue', width  =580, height = 500);
+    gameCanvas.grid(row = 0, column = 0);
+    
+    scoreboardFrame = Frame(root);
+    scoreboardFrame.grid(row =0, column = 1);
+    
+    turnIndicatorRow = 0;
+    
+    turnIndicatorLabel = Label(scoreboardFrame, text = "Red Player's\n Turn", fg = 'red', font = 'times 50');
+    turnIndicatorLabel.grid(row = turnIndicatorRow, column = 0, columnspan = 2);
+    
+    
+    scoreLabelsRow = 1;
+    
+    redScoreLabel = Label(scoreboardFrame, text = "0", fg = 'red', font = 'times 80');
+    redScoreLabel.grid(row = scoreLabelsRow, column = 0);
+    
+    yellowScoreLabel = Label(scoreboardFrame, text = "0", fg = 'yellow', font = 'times 80');
+    yellowScoreLabel.grid(row = scoreLabelsRow, column = 1);
+    
+    
+    
+    
+    returnToMenuButton = Button(scoreboardFrame, text = "Menu", font = 'times 40');
+    returnToMenuButton.grid(row = 2, column = 0, columnspan = 2);
+    
+    circleSize = 30;
+    
+    
     def __init__(self, num_players):
-        root = Tk();
-        root.title("Connect Four");
         
-        gameFrame = Frame(root);
-        gameFrame.grid(row = 0, column = 0);
+        self.controller = ConnectFourController(ConnectFourModel());
         
         
-        gameCanvas = Canvas(gameFrame, bg = 'blue', width  =580, height = 500);
-        gameCanvas.grid(row = 0, column = 0);
+        
+        
+        
+        
+        
+        
         
         for x in range(7):
             for y in range(6):
                 xCoord  = x* 80 + 50;
                 yCoord = y*80 + 50;
-                circleSize = 30;
-                gameCanvas.create_oval(xCoord - circleSize,yCoord -circleSize, xCoord +circleSize, yCoord +circleSize, outline = "#000", fill = 'white', width = 2);
-        
-        
-        
-        scoreboardFrame = Frame(root);
-        scoreboardFrame.grid(row =0, column = 1);
-        
-        turnIndicatorRow = 0;
-        
-        turnIndicatorLabel = Label(scoreboardFrame, text = "Red Player's\n Turn", fg = 'red', font = 'times 50');
-        turnIndicatorLabel.grid(row = turnIndicatorRow, column = 0, columnspan = 2);
-        
-        
-        scoreLabelsRow = 1;
-        
-        redScoreLabel = Label(scoreboardFrame, text = "0", fg = 'red', font = 'times 80');
-        redScoreLabel.grid(row = scoreLabelsRow, column = 0);
-        
-        yellowScoreLabel = Label(scoreboardFrame, text = "0", fg = 'yellow', font = 'times 80');
-        yellowScoreLabel.grid(row = scoreLabelsRow, column = 1);
+                
+                self.gameCanvas.create_oval(xCoord - self.circleSize,yCoord -self.circleSize, xCoord +self.circleSize, yCoord +self.circleSize, outline = "#000", fill = 'white', width = 2);
         
         
         
         
-        returnToMenuButton = Button(scoreboardFrame, text = "Menu", font = 'times 40');
-        returnToMenuButton.grid(row = 2, column = 0, columnspan = 2);
         
-        root.mainloop();
+        
+        
+        self.root.mainloop();
         
         pass
 
@@ -91,8 +116,53 @@ class GameBoardView(tk.Frame):
         """
         
         """
+        
+        #get the grid from the model
+        grid = self.controller.get_board();
+        
+        
+        #for each circle
+        for x in range(6):
+            for y in range(7):
+                
+                #get the color
+                color = grid[x][y];
+                
+                if(color == "e"):
+                    color = 'white';
+                elif(color =="r"):
+                    color = 'red';
+                elif(color == "b"):
+                    color = 'black';
+                
+                
+                
+                #create circle of said color
+                xCoord  = x* 80 + 50;
+                yCoord = y*80 + 50;
+                
+                self.gameCanvas.create_oval(xCoord - self.circleSize,yCoord -self.circleSize, xCoord +self.circleSize, yCoord +self.circleSize, outline = "#000", fill = color, width = 2);
+                
         pass
-
+    
+    
+    def canvasClick(self, event):
+        """
+        """
+        #gets the column the user clicked
+        columnClicked = (event.x - 50)/80;
+        
+        #make sure column selection is valid
+        if(columnClicked < 0 or columnClicked > 6):
+            return;
+        
+        #passes the move to the controller
+        self.controller.place_piece(columnClicked, 'red');
+        
+        
+        pass
+    
+    
 class MenuView(tk.Frame):
 
     def __init__(self):
